bda693423bbe376b5e34ccce07619acc9ee28618
Johnley <johnleysaintthomas6@gmail.com>
2025-09-15 14:52:32 -0700

    update

--- app/src/main/java/com/example/toto2/MainActivity.kt
+++ app/src/main/java/com/example/toto2/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.toto2
 
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -14,17 +16,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        val flashcard_Answer = findViewById<TextView>(R.id.flashcard_Answer)
-        val reponse2 = findViewById<TextView>(R.id.reponse2)
-        val reponse3 = findViewById<TextView>(R.id.reponse3)
-        flashcard_Answer.setOnClickListener {
-            Toast.makeText(this, "Mausaise reponse", Toast.LENGTH_SHORT).show()
+        val baseline_visibility = findViewById<ImageView>(R.id.ivToogleAnswers)
+        val ivToogleAnswers = findViewById<LinearLayout>(R.id.ivToogleAnswers)
+        baseline_visibility.setOnClickListener {
+            if (ivToogleAnswers.visibility== View.GONE){
+                ivToogleAnswers.visibility=View.VISIBLE}
+
+            else{ivToogleAnswers.visibility= View.GONE
         }
-        reponse2.setOnClickListener {
-            Toast.makeText(this, "Bonne reponse", Toast.LENGTH_SHORT).show()
-        }
-        reponse3.setOnClickListener {
-            Toast.makeText(this, "Mauvaise reponse", Toast.LENGTH_SHORT).show()
         }
     }
 }
--- app/src/main/res/layout/README.md
+++
@@ -1,4 +0,0 @@
-Mon projet android
-Bienvenue dans mon projet Android !
-voici une petite demo
-![Animation.gif](../../../../../../../Desktop/Animation.gif)
